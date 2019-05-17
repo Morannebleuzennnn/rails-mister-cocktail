27fa8591ad8f0fcc1483dfda97ab6efc73b2ea68
MoranneBleuzen <moranne.bleuzen@kedgebs.com>
2019-05-17 14:48:18 +0200

    cocktail_controller done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -9,10 +9,18 @@ class CocktailsController < ApplicationController
   end
 
   def new
-
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to(cocktails_path)
   end
 
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
